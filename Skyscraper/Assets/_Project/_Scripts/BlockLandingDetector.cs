fd17e994cf7a5e04d98c6c57a05b3395d4d0fbdd
ZatolochnyiD <d.zatolochnyi@gmail.com>
2025-06-18 10:37:36 +0300

    Made object detect when it stopped its movement and spawn blocks when previous block completely stopped.

---
+++ Skyscraper/Assets/_Project/_Scripts/BlockLandingDetector.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using ThreeDent.EventBroker;
+using System.Collections;
+
+public class BlockLandingDetector : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D physicalBody;
+    [SerializeField] private float speedEpsilon = 0.0002f;
+    [SerializeField] private float timeToStandNotMoved = 4f;
+
+    private bool firstCollisionHappened;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!firstCollisionHappened)
+        {
+            // Object hit other object for the first time.
+            // Should initiate object movement check to ensure it actually landed and not just hit other object.
+            firstCollisionHappened = true;
+            StartCoroutine(MovementCheckCycle());
+        }
+    }
+
+    private IEnumerator MovementCheckCycle()
+    {
+        float timeElapsed = 0f;
+        while (true)
+        {
+            yield return null;
+
+            float currentSpeed = physicalBody.linearVelocity.magnitude + physicalBody.angularVelocity;
+            if (currentSpeed <= speedEpsilon)
+                timeElapsed += Time.deltaTime;
+            if (timeElapsed >= timeToStandNotMoved)
+                break;
+        }
+        EventBroker.Invoke<BlockLandedEvent>();
+    }
+}
+
+public class BlockLandedEvent : IEvent {}
--- Skyscraper/Assets/_Project/_Scripts/BlocksSpawner.cs
+++ Skyscraper/Assets/_Project/_Scripts/BlocksSpawner.cs
@@ -1,3 +1,4 @@
+using ThreeDent.EventBroker;
 using ThreeDent.Helpers.Extensions;
 using UnityEngine;
 
@@ -5,16 +6,27 @@ public class BlocksSpawner : MonoBehaviour
 {
     [SerializeField] private Transform spawnPoint;
     [SerializeField] private GameObject blockToSpawn;
-    [SerializeField] private float spawnInterval;
+    [SerializeField] private float additionalSpawnInterval = 1f;
 
     void Awake()
     {
-        this.InvokeRepeatedly(Spawn, spawnInterval);
+        Spawn();
+        EventBroker.Subscribe<BlockLandedEvent>(SpawnAfterInterval);
+    }
+
+    void OnDestroy()
+    {
+        EventBroker.Unsubscribe<BlockLandedEvent>(SpawnAfterInterval);
     }
 
     private void Spawn()
     {
         var newBlock = Instantiate(blockToSpawn, transform);
         newBlock.transform.position = spawnPoint.position;
+    }
+
+    private void SpawnAfterInterval()
+    {
+        this.InvokeOnce(Spawn, additionalSpawnInterval);
     }
 }
