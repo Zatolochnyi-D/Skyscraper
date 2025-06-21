56f116358cb603a16db86ea0f658533326e68a9d
ZatolochnyiD <d.zatolochnyi@gmail.com>
2025-06-21 18:01:33 +0300

    Moved block logic to separate folder.

--- Skyscraper/Assets/_Project/_Scripts/_BlockLogic/BlockLandingDetector.cs
+++ Skyscraper/Assets/_Project/_Scripts/_BlockLogic/BlockLandingDetector.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
 using ThreeDent.EventBroker;
 using System.Collections;
+using System;
 
 public class BlockLandingDetector : MonoBehaviour
 {
     private static readonly BlockLandedEvent eventArgs = new();
+
+    public event Action OnBlockLanded;
 
     [OnThis, SerializeField] private Rigidbody2D physicalBody;
     [SerializeField] private float speedEpsilon = 0.0002f;
@@ -37,6 +40,7 @@ public class BlockLandingDetector : MonoBehaviour
                 break;
         }
         eventArgs.sender = gameObject;
+        OnBlockLanded?.Invoke();
         EventBroker.Invoke(eventArgs);
     }
 }
