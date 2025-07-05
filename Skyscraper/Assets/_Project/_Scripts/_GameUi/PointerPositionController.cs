3b4195dc74613d3e77a3f629e34a1ec0a5db6252
ZatolochnyiD <d.zatolochnyi@gmail.com>
2025-07-05 12:07:45 +0300

    Made pointer follow falling block.

--- Skyscraper/Assets/_Project/_Scripts/_GameUi/PointerPositionController.cs
+++ Skyscraper/Assets/_Project/_Scripts/_GameUi/PointerPositionController.cs
@@ -1,10 +1,14 @@
 using System.Collections;
+using ThreeDent.Helpers.Extensions;
 using ThreeDent.Helpers.Tools;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PointerPositionController : Singleton<PointerPositionController>
 {
+    [SerializeField] private RectTransform parentCanvasTransform;
     [SerializeField] private RectTransform pointer;
+    [SerializeField] private float offset;
 
     private Transform currentActiveBlock;
     private Coroutine trackingCoroutine;
@@ -23,6 +27,27 @@ public class PointerPositionController : Singleton<PointerPositionController>
 
     private IEnumerator TrackingCycle()
     {
-        yield return null;
+        while (true)
+        {
+            // Calculate how much canvas pixels are in 1 world unit.
+            //   Calculate aspect ratio.
+            //   Find how much world units carema takes on X axis.
+            //   Calcucale how much pixels it takes to pass 1 world unit.
+            var cameraWorldUnitsOnX = Camera.main.orthographicSize * Camera.main.aspect * 2f;
+            var canvasX = parentCanvasTransform.sizeDelta.x;
+            var canvasPixelsPerUnit = canvasX / cameraWorldUnitsOnX;
+
+            var maxX = canvasX / 2 - offset;
+            var minX = -maxX;
+
+            var cameraX = Camera.main.transform.position.x;
+            var blockX = currentActiveBlock.position.x;
+            var pixels = (blockX - cameraX) * canvasPixelsPerUnit;
+            pixels = Mathf.Min(maxX, pixels);
+            pixels = Mathf.Max(minX, pixels);
+            pointer.anchoredPosition = pointer.anchoredPosition.With(x: pixels);
+
+            yield return null;
+        }
     }
 }
