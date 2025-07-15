8fee445aa7c0ee123822bc5b0f1b13f58f7899aa
ZatolochnyiD <d.zatolochnyi@gmail.com>
2025-07-15 12:40:03 +0300

    Created block selector.

---
+++ Skyscraper/Assets/_Project/_Scripts/_GameUi/InventorySelectionController.cs
@@ -0,0 +1,42 @@
+using ThreeDent.DevelopmentTools.Attributes;
+using TMPro;
+using UnityEngine;
+
+public class InventorySelectionController : MonoBehaviour
+{
+    [IsChild(1), SerializeField] private GameObject sampleText;
+    [SerializeField] private GameObject[] blocks;
+    [SerializeField] private string[] names;
+
+    private RectTransform[] textTransforms;
+    private TextMeshProUGUI[] texts;
+    private int currentSelection = 0;
+
+    void Awake()
+    {
+        sampleText.SetActive(false);
+
+        textTransforms = new RectTransform[blocks.Length];
+        texts = new TextMeshProUGUI[blocks.Length];
+        for (int i = 0; i < blocks.Length; i++)
+        {
+            var newText = Instantiate(sampleText, sampleText.transform.parent);
+            newText.SetActive(true);
+            texts[i] = newText.GetComponent<TextMeshProUGUI>();
+            texts[i].text = names[i];
+            textTransforms[i] = newText.GetComponent<RectTransform>();
+        }
+
+        SetSelection(0);
+    }
+
+    private void SetSelection(int newSelection)
+    {
+        textTransforms[currentSelection].sizeDelta = new(0f, 50f);
+        texts[currentSelection].fontSize = 36;
+
+        currentSelection = newSelection;
+        textTransforms[currentSelection].sizeDelta = new(0f, 100f);
+        texts[currentSelection].fontSize = 72;
+    }
+}
