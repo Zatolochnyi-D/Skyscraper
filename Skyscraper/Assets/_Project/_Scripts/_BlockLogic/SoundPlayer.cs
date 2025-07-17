a871cfee488a8ea6023c0e6d518d5b4162e46056
ZatolochnyiD <d.zatolochnyi@gmail.com>
2025-07-17 15:09:12 +0300

    Fixed bad naming.

--- Skyscraper/Assets/_Project/_Scripts/_BlockLogic/SoundPlayer.cs
+++ Skyscraper/Assets/_Project/_Scripts/_BlockLogic/SoundPlayer.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class _SoundPlayer : MonoBehaviour
+public class SoundPlayer : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
