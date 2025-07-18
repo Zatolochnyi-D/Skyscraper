028388f653e454bbd7f011a0b0269dc74df073fd
ZatolochnyiD <d.zatolochnyi@gmail.com>
2025-07-18 15:10:39 +0300

    Created GlobalSound player that should manage how to play sounds from multiple contacts.

---
+++ Skyscraper/Assets/_Project/_Scripts/_BlockLogic/GlobalSoundPlayer.cs
@@ -0,0 +1,10 @@
+using ThreeDent.DevelopmentTools;
+using UnityEngine;
+
+public class GlobalSoundPlayer : Singleton<GlobalSoundPlayer>
+{
+    public void QueueSoundPlayRequest(GameObject requestor, Vector3 position)
+    {
+        
+    }
+}
