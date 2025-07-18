5658a20c7689a1a43952d2eacad5ad65afe6787f
ZatolochnyiD <d.zatolochnyi@gmail.com>
2025-07-18 15:03:44 +0300

    Added sound player to block prefab.

--- Skyscraper/Assets/_Project/_Scripts/_BlockLogic/SoundPlayer.cs
+++ Skyscraper/Assets/_Project/_Scripts/_BlockLogic/SoundPlayer.cs
@@ -1,16 +1,25 @@
+using ThreeDent.DevelopmentTools.Option;
+using ThreeDent.Helpers.Extensions;
 using UnityEngine;
 
 public class SoundPlayer : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    // [SerializeField] private AudioClip[] hitSounds;
+    // [SerializeField] private AudioClip fallSound;
+    [SerializeField] private LayerMask ignoreCollisionLayers;
 
-    // Update is called once per frame
-    void Update()
+    // void Start()
+    // {
+    //     this.InvokeOnce(() => AudioPlayer.Instance.PlayDistant2d(hitSounds[0], Vector2.left * 9f, maxDistance: 10f), 2f);
+    //     this.InvokeOnce(() => AudioPlayer.Instance.PlayDistant2d(hitSounds[0], Vector2.zero, maxDistance: 10f), 3f);
+    //     this.InvokeOnce(() => AudioPlayer.Instance.PlayDistant2d(hitSounds[0], Vector2.right * 9f, maxDistance: 10f), 4f);
+    // }
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.otherCollider.gameObject.layer != ignoreCollisionLayers)
+        {
+            Debug.Log("Block contact");
+        }
     }
 }
