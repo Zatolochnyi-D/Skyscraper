97cfe10cb570bb3b5203f910b98d05b453477b8c
ZatolochnyiD <d.zatolochnyi@gmail.com>
2025-08-05 11:14:54 +0300

    Added music for menu and game.

--- Skyscraper/Assets/_Project/_Scripts/_GameUi/TutorialUi.cs
+++ Skyscraper/Assets/_Project/_Scripts/_GameUi/TutorialUi.cs
@@ -4,6 +4,9 @@ using UnityEngine.UI;
 
 public class TutorialUi : MonoBehaviour
 {
+    private const string DontShowAgainSave = "DNSA";
+
+    [SerializeField] private Toggle dontShowAgainToggle;
     [SerializeField] private Button hideButton;
 
     private void Awake()
@@ -13,12 +16,20 @@ public class TutorialUi : MonoBehaviour
             gameObject.SetActive(false);
             Time.timeScale = 1f;
             InputManager.SwitchMode(InputModes.Game);
+            PlayerPrefs.SetInt(DontShowAgainSave, dontShowAgainToggle.isOn ? 1 : 0);
         });
     }
 
     private void Start()
     {
-        Time.timeScale = 0f;
-        InputManager.SwitchMode(InputModes.None);
+        if (PlayerPrefs.GetInt(DontShowAgainSave, 0) == 0)
+        {
+            Time.timeScale = 0f;
+            InputManager.SwitchMode(InputModes.None);
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
     }
 }
